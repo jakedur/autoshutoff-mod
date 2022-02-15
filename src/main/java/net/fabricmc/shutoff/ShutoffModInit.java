8dad074f1cfb3bc1b21b2f34fe24ec0c55d5def2
Jake Durham <jakedur@gmail.com>
2022-02-15 14:17:15 -0700

    Updated for OS detection and config for custom time to wait

--- src/main/java/net/fabricmc/shutoff/ShutoffModInit.java
+++ src/main/java/net/fabricmc/shutoff/ShutoffModInit.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class ShutoffModInit implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -14,13 +15,25 @@ public class ShutoffModInit implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
+	// Load config 'shutoff.properties', if it isn't present create one
+	SimpleConfig CONFIG = SimpleConfig.of( "shutoff" ).provider( this::provider ).request();
+
+	// Custom config provider, returns the default config content
+	// if the custom provider is not specified SimpleConfig will create an empty file instead
+	private String provider( String filename ) {
+		return "Wait-Time-Minutes=30";
+	}
+
+	public final int Wait = CONFIG.getOrDefault( "Wait-Time-Minutes", 30 );
+
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		ServerTickEvents.END_SERVER_TICK.register(new ShutoffModRuntime());
+		ServerTickEvents.END_SERVER_TICK.register(new ShutoffModRuntime(Wait));
 		
 		LOGGER.info("Loaded Shutoff Mod");
 	}
--- src/main/java/net/fabricmc/shutoff/ShutoffModRuntime.java
+++ src/main/java/net/fabricmc/shutoff/ShutoffModRuntime.java
@@ -7,18 +7,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class ShutoffModRuntime implements ServerTickEvents.EndTick {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("modid");
     int minSinceLastPlayer = 0;
+    int Wait;
+
+    public ShutoffModRuntime(int Wait) {
+        this.Wait = Wait*20*60; //convert minutes to ticks
+    }
 
     public void shutoff() throws IOException {
         try {
             LOGGER.info("Suspending Computer");
-            String[] cmd = { "/bin/bash", "-c", "sudo /bin/systemctl suspend" };
-            Process process = Runtime.getRuntime().exec(cmd);
-            process.waitFor();
+            String OS = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
+            if(OS.contains("win")) {
+                String[] cmd = {"Rundll32.exe powrprof.dll,SetSuspendState Sleep"};
+                Process process = Runtime.getRuntime().exec(cmd);
+                process.waitFor();
+            }
+            else if(OS.contains("mac")) {
+                String[] cmd = {"osascript -e 'tell application \"System Events\" to sleep'"};
+            }
+            else {
+                String[] cmd = {"/bin/bash", "-c", "sudo /sbin/pm-suspend --quirk-none"};
+                Process process = Runtime.getRuntime().exec(cmd);
+                process.waitFor();
+            }
         } catch (InterruptedException e) {
             LOGGER.info(e.getMessage());
         }
@@ -32,11 +49,11 @@ public class ShutoffModRuntime implements ServerTickEvents.EndTick {
         else
             minSinceLastPlayer = 0;
 
-        if(minSinceLastPlayer >= 36000) {
+        if(minSinceLastPlayer >= Wait) {
             try {
                 minSinceLastPlayer = 0;
                 shutoff();
-            } catch (IOException e) {}
+            } catch (IOException e) { LOGGER.info(e.getMessage()); }
         }
     }
 }
